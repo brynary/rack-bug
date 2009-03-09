84f1a1f09a0f70ffdc05fbfe0ac3140874079b4d
Bryan Helmkamp <bryan@brynary.com>
2009-03-09 00:20:02 -0400

    Splitting functionality out of the middleware

--- lib/rack/bug.rb
+++ lib/rack/bug.rb
@@ -4,6 +4,7 @@ unless $LOAD_PATH.include?(File.expand_path(File.dirname(__FILE__) + "/.."))
   $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/.."))
 end
 
+require "rack/bug/toolbar"
 require "rack/bug/panels/timer_panel"
 require "rack/bug/panels/env_panel"
 
@@ -17,44 +18,16 @@ module Rack
       end
     
       def call(env)
-        @env = env
-        @env["rack.bug.panels"] ||= []
-        status, headers, body = builder.call(env)
-        return debugged_response(env, status, headers, body)
-      end
-    
-      def panel_classes
-        [TimerPanel, EnvPanel]
+        toolbar = Toolbar.new(asset_server)
+        toolbar.call(env)
       end
       
-      def builder
-        @builder = Rack::Builder.new
-        
-        panel_classes.each do |panel_class|
-          @builder.use panel_class
-        end
-        
-        dir = ::File.expand_path(::File.dirname(__FILE__) + "/bug/public")
-        @builder.use Rack::Static, :urls => ["/__rack_bug__"], :root => dir
-        @builder.run @app
-        return @builder
+      def asset_server
+        Rack::Static.new(@app, :urls => ["/__rack_bug__"], :root => public_path)
       end
       
-      def debugged_response(env, status, headers, body)
-        output = []
-        body.each do |body_fragment|
-          output << body_fragment
-        end
-        full_output = output.join
-        full_output.sub! /<body>/, "<body>" + html(env)
-        headers["Content-Length"] = full_output.size.to_s
-        return [status, headers, full_output]
-      end
-      
-      def html(env)
-        @panels = @env["rack.bug.panels"].reverse
-        @template = ERB.new ::File.read(::File.dirname(__FILE__) + "/bug/views/bug.html.erb")
-        @template.result(binding)
+      def public_path
+        ::File.expand_path(::File.dirname(__FILE__) + "/bug/public")
       end
       
     end
--- lib/rack/bug/toolbar.rb
+++ lib/rack/bug/toolbar.rb
@@ -3,19 +3,46 @@ module Rack
     
     class Toolbar
       
+      def initialize(app)
+        @app = app
+      end
+      
+      def call(env)
+        @env = env
+        @env["rack.bug.panels"] ||= []
+        
+        status, headers, body = builder.call(env)
+        response = Rack::Response.new(body, status, headers)
+        
+        inject_into(response)
+        return response.to_a
+      end
+      
+      def builder
+        builder = Rack::Builder.new
+        panel_classes.each do |panel_class|
+          builder.use panel_class
+        end
+        builder.run @app
+        return builder
+      end
+      
       def panel_classes
         [TimerPanel, EnvPanel]
       end
       
-      def builder
-        @builder = Rack::Builder.new
+      def inject_into(response)
+        full_body = response.body.join
+        full_body.sub! /<\/body>/, render + "</body>"
         
-        panel_classes.each do |panel_class|
-          @builder.use panel_class
-        end
-        
-        @builder.run @app
-        return @builder
+        response["Content-Length"] = full_body.size.to_s
+        response.body = [full_body]
+      end
+      
+      def render
+        @panels = @env["rack.bug.panels"].reverse
+        @template = ERB.new ::File.read(::File.dirname(__FILE__) + "/../bug/views/bug.html.erb")
+        @template.result(binding)
       end
       
     end
