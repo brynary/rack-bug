b824ddc0ca4d40daaa2c79f1a30d0db19ee4d0cf
Bryan Helmkamp <bryan@brynary.com>
2009-03-26 11:50:54 -0400

    Fix method names

--- lib/rack/bug/extensions/actionview_extension.rb
+++ lib/rack/bug/extensions/actionview_extension.rb
@@ -1,12 +1,12 @@
 if defined?(ActionView) && defined?(ActionView::Template)
   ActionView::Template.class_eval do
 
-    def render_template_with_rack_test(*args, &block)
+    def render_template_with_rack_bug(*args, &block)
       Rack::Bug::TemplatesPanel.record(path_without_format_and_extension) do
-        render_template_without_rack_test(*args, &block)
+        render_template_without_rack_bug(*args, &block)
       end
     end
 
-    alias_method_chain :render_template, :rack_test
+    alias_method_chain :render_template, :rack_bug
   end
 end
