af36df146407d2a27480d586866a9d7cec0d80e6
Tim Connor <timocratic@gmail.com>
2010-03-27 11:15:26 -0700

    consistency with filename and module name

--- lib/rack/bug/panels/log_panel.rb
+++ lib/rack/bug/panels/log_panel.rb
@@ -1,4 +1,4 @@
-require "rack/bug/panels/log_panel/rails_extension"
+require "rack/bug/panels/log_panel/logger_extension"
 
 module Rack
   class Bug
--- lib/rack/bug/panels/log_panel/logger_extension.rb
+++ lib/rack/bug/panels/log_panel/logger_extension.rb
@@ -1,5 +1,5 @@
 if defined?(Rails) && Rails.respond_to?(:logger) && Rails.logger
-  module LoggingExtensions
+  module LoggerExtension
     def add(*args, &block)
       logged_message = super
       Rack::Bug::LogPanel.record(logged_message, args[0])
@@ -7,5 +7,5 @@ if defined?(Rails) && Rails.respond_to?(:logger) && Rails.logger
     end
   end
 
-  Rails.logger.extend LoggingExtensions
+  Rails.logger.extend LoggerExtensions
 end
