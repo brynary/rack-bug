6d58164d9f17f1add182c0afff36afa919d5952f
Judson <nyarly@gmail.com>
2011-08-16 16:42:11 -0700

    TemplatePanel cleaned up

--- lib/rack/bug/panels/templates_panel.rb
+++ lib/rack/bug/panels/templates_panel.rb
@@ -21,7 +21,7 @@ module Rack
 
 
       def request_start(env, start)
-        @current = root_rendering
+        @current = Rendering.new("root")
       end
 
       def request_finish(env, status, headers, body, timing)
@@ -40,41 +40,6 @@ module Rack
       def after_detect(method_call, timing, args, result)
         @current.timing = timing
         @current = @current.parent
-      end
-
-      def total_time
-        root_rendering.duration
-      end
-
-      def root_rendering
-        @root_rendering ||= Rendering.new("root")
-      end
-
-      #XXX ???
-      def add(template_name, start_time, end_time, event)
-        current = Rendering.new(template_name, start_time, end_time)
-        root_rendering.children.each do |child|
-          next unless event.parent_of?(child)
-          root_rendering.delete(child)
-          current.add(child)
-        end
-        root_rendering.add(current)
-      end
-
-      #XXX ???
-      def self.record_event(event)
-        return unless Rack::Bug.enabled?
-
-        template_description = "#{event.name}: #{event.payload[:virtual_path] || event.payload[:identifier]}"
-        template_trace.add(template_description, event.time, event.end, event)
-      end
-
-      def self.reset
-        Thread.current["rack.bug.template_trace"] = Trace.new
-      end
-
-      def self.template_trace
-        Thread.current["rack.bug.template_trace"] ||= Trace.new
       end
 
       def name
