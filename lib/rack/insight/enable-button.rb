b40b3cb3afcd8091dd1685b7302d2efedefe6e75
bibendi <bibendi@bk.ru>
2014-07-08 18:36:52 +0600

    fix inject body if body empty
    Closes PC4-13514'

--- lib/rack/insight/enable-button.rb
+++ lib/rack/insight/enable-button.rb
@@ -13,11 +13,14 @@ module Rack::Insight
       @env = env
       status, headers, body = @app.call(@env)
 
-      response = Rack::Response.new(body, status, headers)
+      if body.present?
+        response = Rack::Response.new(body, status, headers)
+        inject_button(response) if okay_to_modify?(env, response)
 
-      inject_button(response) if okay_to_modify?(env, response)
-
-      return response.to_a
+        response.to_a
+      else
+        [status, headers, body]
+      end
     end
 
     def okay_to_modify?(env, response)
